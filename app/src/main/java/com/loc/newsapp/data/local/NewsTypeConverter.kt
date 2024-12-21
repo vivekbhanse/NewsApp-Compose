3d8236e86a850e58660a881869359212bf462fe0
vivek bhanse <you@example.com>
2024-12-21 19:18:23 +0530

    adding Room DB 2

--- app/src/main/java/com/loc/newsapp/data/local/NewsTypeConverter.kt
+++ app/src/main/java/com/loc/newsapp/data/local/NewsTypeConverter.kt
@@ -1,19 +1,19 @@
 package com.loc.newsapp.data.local
 
 import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import com.loc.newsapp.domain.models.Source
 
 
-@ProvidedTypeConverter
 class NewsTypeConverter {
 
-    @TypeConverters
+    @TypeConverter
     fun sourceToString(source: Source) : String{
         return "${source.id}, ${source.name}"
     }
 
-    @TypeConverters
+    @TypeConverter
     fun stringToSource(source: String):Source{
         return source.split(",").let { sourceArray ->
             Source(sourceArray[0],sourceArray[1])
